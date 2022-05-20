31f7c21252de2ed9beb973fcedf05c9e525b6597
AlessioIncicco96 <105919769+AlessioIncicco96@users.noreply.github.com>
2022-05-20 14:06:37 +0200

    Update streamlit_app.py
    i added these 2 rows:
    #let's put a pick list here, so they can pick the fruit they want to include
    streamlit.multiselect("Pick some fruits:",list(My_fruit_list.index))

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,9 @@ streamlit.text ('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭Build Your Onw Fruit Smoothie🥝🍇')
 
 My_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#let's put a pick list here, so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits:",list(My_fruit_list.index))
+
+#display the table on the page
 streamlit.dataframe(My_fruit_list)
